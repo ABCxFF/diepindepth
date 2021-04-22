f64d06873e8ca77a4c8f638b70ffb43930278eed
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-04-22 08:17:41 -0400

    Update AbstractFieldGroup.c

--- memory/structs/AbstractFieldGroup.c
+++ memory/structs/AbstractFieldGroup.c
@@ -1,18 +1,18 @@
 struct AbstractFieldGroup
 {
     // Self Ent Pointer @00
-    // - points to its entity
-    int32_t self;
+    // - points to its owner
+    struct AbstractEntity* self;
 
-    // Field Ids
+    // Field State Managers
     // - *n* size list of fields part of the field group
-    // - Unknown purpose of this list, but it is trivial in the unshuffling of fields
-    uint8_t fields[n];
+    // - Bytes for the sole purpose of managing a field's state (not really relevant I think on the client)
+    uint8_t field_state_managers[n];
 
     // Field Data
     // - In order of the shuffled fields order
     // - Varying data types and values
-    int32_t data[n]
+    int32_t field_data[n]
 
     // and some more stuff over yonder 👀
 }
