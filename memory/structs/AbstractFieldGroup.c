6f4d7dfb2411cc9a40fa551ebf0dfa781d0f9397
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-04-22 10:46:01 -0400

    shad acceptance

--- memory/structs/AbstractFieldGroup.c
+++ memory/structs/AbstractFieldGroup.c
@@ -1,3 +1,24 @@
+/*
+    Abstract Entity manages all data relating to an entity
+   `AbstractEntity` size depends on their type
+
+ID : NAME      : SIZE
+0 : RELATIONS : 38
+2 : BARREL    : 48
+3 : PHYSICS   : 108
+4 : HEALTH    : 76
+6 : UNUSED    :
+7 : ARENA     : 684
+8 : NAME      : 28
+9 : GUI       : 482
+10: POS       : 152
+11: STYLE     : 112
+13: SCORE     : 36
+14: TEAM      : 36
+(credits to Peruna)
+
+    In Emscripten, Pointers are 32 bit.
+*/
 struct AbstractFieldGroup
 {
     // Self Ent Pointer @00
@@ -16,20 +37,3 @@ struct AbstractFieldGroup
 
     // and some more stuff over yonder 👀
 }
-
-sizeof();
-/* (thanks peruna)
-  //ID : NAME      : SIZE
-  // 0 : RELATIONS : 38
-  // 2 : BARREL    : 48
-  // 3 : PHYSICS   : 108
-  // 4 : HEALTH    : 76
-  // 6 : UNUSED    :
-  // 7 : ARENA     : 684
-  // 8 : NAME      : 28
-  // 9 : GUI       : 482
-  // 10: POS       : 152
-  // 11: STYLE     : 112
-  // 13: SCORE     : 36
-  // 14: TEAM      : 36
-*/
