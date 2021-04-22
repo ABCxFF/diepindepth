0e340ce7278f7979dd1f3fc41abd715a3e745081
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-04-22 10:49:03 -0400

    Update vector.c

--- memory/structs/vector.c
+++ memory/structs/vector.c
@@ -1,3 +1,10 @@
+/*
+    Vectors store elements of data efficiently(?).
+    Vectors are 12 bytes of size
+    
+    In Emscripten, Pointers are 32 bit.
+*/
+
 // Explains itself? Kind of a standard way of storing data, but this is its format in emscripten compiled binaries
 struct vector {
     uint32_t startPtr;
@@ -6,5 +13,3 @@ struct vector {
 
     uint32_t endPtrCapacity;
 }
-
-sizeof(vector) == 12;
