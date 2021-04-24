6a51c2b6f2b640f96e02156cbe58edf4fdf2fc07
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-04-23 22:06:36 -0400

    up

--- memory/structs/Achievement.c
+++ memory/structs/Achievement.c
@@ -1,6 +1,24 @@
 // All I know, size: 168
 
+/* Example Achievement Definition
+{
+  "name": "A moment to cherish forever",
+  "desc": "Destroy your first tank",
+  "conds": [
+    {
+      "event": "kill",
+      "tags": {
+        "victim.isTank": true
+      }
+    }
+  ]
+}
+*/
+
 struct Achievement
 {
-
+  int32_t pointer; // idk where or why it points, but it does
+  
+  struct cstr name;
+  struct cstr desc;
 }
