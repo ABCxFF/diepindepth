2f1e6c27e19e05680abb58b42dc43eba0ac3aa18
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-04-24 17:13:53 -0400

    Create Achievement2.c

---
+++ memory/structs/Achievement2.c
@@ -0,0 +1,23 @@
+
+int something = 115400; // $vector
+/* Example Achievement Definition
+{
+  "name": "A moment to cherish forever",
+  "desc": "Destroy your first tank",
+  "conds": [
+    {
+      "event": "kill",
+      "tags": {
+        "victim.isTank": true
+      }
+    }
+  ]
+}
+*/
+
+struct Achievement
+{
+  struct cstr name;
+  struct str desc;
+}
+
