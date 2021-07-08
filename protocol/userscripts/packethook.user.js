2866ecf2c63cee9247111b62c83ffd1e01e997f2
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-07-08 18:42:47 -0400

    2c6859c7cfee4401fe047256f1e2aa832d7c524a + reverted

--- protocol/userscripts/packethook.user.js
+++ protocol/userscripts/packethook.user.js
@@ -2,8 +2,8 @@
 // @name         Diep.io Packet WASM Hook
 // @author       ABC
 // @version      1.0.0
-// @namespace    87b52c1f18e340268e65e58b67b565bfae4fc8a0
-// @description  87b52c1f18e340268e65e58b67b565bfae4fc8a0
+// @namespace    2c6859c7cfee4401fe047256f1e2aa832d7c524a
+// @description  2c6859c7cfee4401fe047256f1e2aa832d7c524a
 // @match        *://diep.io/
 // @run-at       document-start
 // @require      https://raw.githubusercontent.com/Qwokka/wail.min.js/5e32d36bd7a5e0830d1ff4b64d3587aea13f77da/wail.min.js
@@ -14,7 +14,7 @@
 
 /*
   Usage is explained in the console on run
-  For build 87b52c1f18e340268e65e58b67b565bfae4fc8a0
+  For build 2c6859c7cfee4401fe047256f1e2aa832d7c524a
   The way this script works will be explained in /memory or /wasm someday, but ignore that for now
 */
 
@@ -23,12 +23,12 @@ const nsfsk = false;
 class PacketHook extends EventTarget {
  static get CONST() {
     return {
-      BUILD: "87b52c1f18e340268e65e58b67b565bfae4fc8a0",
-      SEND_PACKET_INDEX: 105,
-      RECV_PACKET_INDEX: 402,
+      BUILD: "2c6859c7cfee4401fe047256f1e2aa832d7c524a",
+      SEND_PACKET_INDEX: 106,
+      RECV_PACKET_INDEX: 403,
       MALLOC: 'R',
-      FREE: 'v',
-      SOCKET_PTR: 103332,
+      FREE: 'I',
+      SOCKET_PTR: 103860,
     }
   }
   
@@ -175,8 +175,8 @@ if (!nsfsk) throw "Packet Hook";
 
 const TYPE = ['incoming', 'outgoing']
 
-window.pHook = new PacketHook(function(type, ptr, len) {
-  pHook.dispatchEvent(new MessageEvent(TYPE[type], {data: Hook.HEAPU8.slice(ptr, ptr+len)}))
+window.Hook = new PacketHook(function(type, ptr, len) {
+  Hook.dispatchEvent(new MessageEvent(TYPE[type], {data: Hook.HEAPU8.slice(ptr, ptr+len)}))
   
   return 0;
 });
