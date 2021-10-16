b6a146ac77ebfa5786e7593a09a6469f5144753e
ABC <79597906+ABCxFF@users.noreply.github.com>
2021-10-16 08:12:12 -0400

    Update vector.h

--- memory/structs/vector.h
+++ memory/structs/vector.h
@@ -7,9 +7,9 @@
 
 // Explains itself? Kind of a standard way of storing data, but this is its format in emscripten compiled binaries
 struct vector {
-    void* start; 
+    void* start; // Points to the start of the elements contained - @00
 
-    void* end; // Denotes how much of the total space is used (end - start)
+    void* end; // Denotes how much of the total space is used (end - start) - @04
 
-    void* end_capacity; // Denotes the absolute space that the vector has allocated (end_capacity - start)
+    void* end_capacity; // Denotes the absolute space that the vector has allocated (end_capacity - start) - @08
 };
