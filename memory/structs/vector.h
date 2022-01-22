c0698b1357064c28f9b83f94250f30ebfa13b65d
Shubham Dhar <61672298+ALPH2H@users.noreply.github.com>
2022-01-21 19:33:54 -0500

    Update vector.h

--- memory/structs/vector.h
+++ memory/structs/vector.h
@@ -1,11 +1,12 @@
 /*
-    Vectors store elements of data efficiently(?).
+    Vectors store elements of data. They  provide constant access time to each element via their index. 
     Vectors are 12 bytes of size
     
     In Emscripten, Pointers are 32 bit.
 */
 
-// Explains itself? Kind of a standard way of storing data, but this is its format in emscripten compiled binaries
+// Explains itself? Kind of a standard way of storing data, but this is its format in emscripten compiled binaries.
+// The start of the elements is allocated away from the vector and does not begin after the pointer to end_capacity
 struct vector {
     void* start; // Points to the start of the elements contained - @00
 
