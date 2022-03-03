0bcd9f6d1b4f5d86b3b4046ef05c34e8a206a087
ABC <79597906+ABCxFF@users.noreply.github.com>
2022-03-03 12:16:18 -0500

    Update EHandle.cpp

--- wasm/rev/EHandle.cpp
+++ wasm/rev/EHandle.cpp
@@ -10,7 +10,7 @@ EHandle::EHandle() {
 
 // Constructs an EHandle out of an entity
 EHandle::EHandle(Entity const* entity) {
-    if (entity == NULL) {
+    if (entity == nullptr) {
         simulationId = 0;
         id = 0;
         hash = 0;
@@ -32,22 +32,22 @@ void EHandle::Decode(Simulation* simulation, BinView& view) {
     // The following assertion is - in the actual source code - being done on line 367 in file `shared/Entity.cpp`
     assert(hash <= 1 << 14);
     if (hash == 0) return;
-    id = NextVarUint32();
+    id = view.NextVarUint32();
     // The following assertion is - in the actual source code - being done on line 370 in file `shared/Entity.cpp`
     assert(id < 16384);
 }
 
 // Gets the entity being referenced by this EHandle
 Entity* EHandle::Deref() const {
-    if (hash == 0) return NULL;
+    if (hash == 0) return nullptr;
  
     Simulation* simulation = Simulation::GetSimulation(simulationId);
-    if (simulation == NULL) return NULL;
+    if (simulation == nullptr) return nullptr;
     EntityManager* entities = simulation->Entities();
     // The following assertion is - in the actual source code - being done on line 19 in file `shared/EntityManager.cpp`
     assert(id < 16384);
     
     Entity* entity = entities->GetEntity(id);
-    if (simulation->IDManager->ids[id] + entity->id == 0 || entity->id != id) return NULL;
+    if (simulation->IDManager->ids[id] + entity->id == 0 || entity->id != id) return nullptr;
     return entity;
 }
